253896b6b1423f42e48335649c2ceb5b5f18d8e6
Eric Biewener <eric@talentpair.com>
2018-07-04 09:45:27 -0700

    Regex work

--- src/cacher.js
+++ src/cacher.js
@@ -1,88 +1,51 @@
 const fs = require('fs-extra')
 const _ = require('lodash')
-const {parseLineImportPath, isPathPackage, getLineImports} = require('./utils')
+const {isPathPackage} = require('./utils')
+const {parseImports} = require('./regex')
 
 function cacheFile(plugin, filepath, data = {_extraImports: {}}) {
+  const fileText = fs.readFileSync(filepath, 'utf8')
+  const imports = parseImports(fileText)
+
+  for (const importData of imports) {
+    if (isPathPackage(plugin, importData.path)) {
+      const existing = data._extraImports[importData.path] || {}
+      data._extraImports[importData.path] = existing
+      if (importData.imports) {
+        existing.exports = _.union(existing.exports, importData.imports)
+      } else {
+        existing.importEntirePackage = true;
+      }
+    }
+    // If there are imports, than they'll get added to the cache when that file gets cached.
+    else if (!importData.imports) {
+      data[importData.path] = {importEntirePackage: true}
+    }
+  }
+
   const classes = []
   const functions = []
   const constants = []
-  let multiLineStart
-  let multiLineUsingParens
+  const lines = fileText.split('\n')
 
-  const lines = fs.readFileSync(filepath, 'utf8').split('\n')
-
-  lines.forEach((line, i) => {
-    // Importing entire module
-    if (line.startsWith('import ')) {
-      const linePath = line.split(' ')[1]
-      
-      if (isPathPackage(plugin, linePath)) {
-        const existing = data._extraImports[linePath] || {}
-        existing.importEntirePackage = true;
-        data._extraImports[linePath] = existing
-      } else {
-        data[linePath] = {importEntirePackage: true}
-      }
-
-      return
-    }
-    
-    const isImportStart = line.startsWith('from ')
-    const linePath = isImportStart ? parseLineImportPath(plugin, line) : null
-    if (isImportStart && !isPathPackage(plugin, linePath)) return
-
-    if (isImportStart || multiLineStart != null) {
-    
-      // If multiline, just continue looping until we find the end
-      if (multiLineStart != null) {
-        if (multiLineUsingParens) {
-          if (!line.endsWith(')')) return
-        } else {
-          if (line.endsWith('\\')) return
-        }
-      }
-      // Check if starting multiline
-      else {
-        const hasParens = line.includes(' import (') && !line.endsWith(')')
-        if (hasParens || line.endsWith('\\')) {
-          multiLineStart = i
-          multiLineUsingParens = hasParens
-          return
-        }
-      }
-
-      const importStartLine = isImportStart ? i : multiLineStart
-      const startLinePath = isImportStart ? linePath : parseLineImportPath(plugin, lines[importStartLine])
-      multiLineStart = null
-      
-      const lineImports = getLineImports(lines, importStartLine)
-      const existing = data._extraImports[startLinePath] || {}
-
-      existing.exports = _.union(existing.exports, lineImports)
-      data._extraImports[startLinePath] = existing
-      return
-    }
-
+  for (const line of lines) {
     const words = line.split(' ')
     const word0 = words[0]
     const word1 = words[1]
 
-    // Class
     if (word0 === 'class') {
       classes.push(trimClassOrFn(word1))
     }
-    // Function
     else if (word0 === 'def') {
-      if (!word1.startsWith('_')) {
-        functions.push(trimClassOrFn(word1))
-      }
+      // Don't export private functions
+      if (!word1.startsWith('_')) functions.push(trimClassOrFn(word1))
     }
-    // Constant
     else if (word1 === '=' && word0.toUpperCase() === word0) {
       constants.push(word0)
     }
-  })
+  }
 
+  // This order of class, function, constant will be maintained when picking an import from the list
   const exp = [...classes, ...functions, ...constants]
   if (exp.length) data[plugin.utils.getFilepathKey(filepath)] = { exports: exp }
 
--- src/importer.js
+++ src/importer.js
@@ -2,6 +2,7 @@ const {window} = require('vscode')
 const path = require('path')
 const _ = require('lodash')
 const {getLineImports, parseLineImportPath, isPathPackage} = require('./utils')
+const {parseImports, getLastInitialCommentLine} = require('./regex')
 
 function buildImportItems(plugin, exportData) {
   const {projectRoot, shouldIncludeImport} = plugin
@@ -10,8 +11,13 @@ function buildImportItems(plugin, exportData) {
 
   for (const importPath of Object.keys(exportData).sort()) {
     const data = exportData[importPath]
-    const absImportPath = data.isExtraImport ? importPath : path.join(projectRoot, importPath)
-    if (shouldIncludeImport && !shouldIncludeImport(absImportPath, activeFilepath)) {
+    const absImportPath = data.isExtraImport
+      ? importPath
+      : path.join(projectRoot, importPath)
+    if (
+      shouldIncludeImport &&
+      !shouldIncludeImport(absImportPath, activeFilepath)
+    ) {
       continue
     }
 
@@ -22,11 +28,11 @@ function buildImportItems(plugin, exportData) {
       dotPath = plugin.utils.removeExt(importPath).replace(/\//g, '.')
       if (plugin.processImportPath) dotPath = plugin.processImportPath(dotPath)
     }
-    
+
     if (data.importEntirePackage) {
       items.push({
         label: importPath,
-        isExtraImport: data.isExtraImport,
+        isExtraImport: data.isExtraImport
       })
     }
 
@@ -36,7 +42,7 @@ function buildImportItems(plugin, exportData) {
       items.push({
         label: exportName,
         description: dotPath,
-        isExtraImport: data.isExtraImport,
+        isExtraImport: data.isExtraImport
       })
     }
   }
@@ -50,16 +56,28 @@ function insertImport(plugin, importSelection) {
   const importPath = importSelection.description || exportName
   const editor = window.activeTextEditor
 
-  const lines = editor.document.getText().split('\n')
-  
-  const linePosition = getLinePosition(plugin, importPath, isExtraImport, lines)
-  if (isPackageImport && !linePosition.isFirstImportLine && !linePosition.lineIndexModifier) {
-    window.showErrorMessage('Can\'t import entire package when parts of the package are already being imported.')
+  const fileText = editor.document.getText()
+  const imports = parseImports(fileText)
+  const linePosition = getLinePosition(
+    plugin,
+    importPath,
+    isExtraImport,
+    imports,
+    fileText
+  )
+  if (
+    isPackageImport &&
+    !linePosition.isFirstImportLine &&
+    !linePosition.lineIndexModifier
+  ) {
+    window.showErrorMessage(
+      'Can\'t import entire package when parts of the package are already being imported.'
+    )
     return
   }
-  const lineImports = getNewLineImports(lines, exportName, linePosition)
+  const lineImports = getNewLineImports(imports, exportName, linePosition)
   if (!lineImports) return
-  
+
   let newLine = getNewLine(plugin, importPath, lineImports)
 
   // Import groups
@@ -67,39 +85,51 @@ function insertImport(plugin, importSelection) {
   const {lineIndexModifier} = linePosition
   // If lineIndexModifier is 0, we're adding to a pre-existing line so no need to worry about groups
   if (lineIndexModifier && plugin.importGroups) {
-    const insertPosition = lineIndexModifier < 0 ? linePosition.start - 1 : linePosition.start
+    const insertPosition =
+      lineIndexModifier < 0 ? linePosition.start - 1 : linePosition.start
     const surrounding = getSurroundingImportPaths(plugin, lines, insertPosition)
-    
+
     if (surrounding.before || surrounding.after) {
-      const beforeGroup = surrounding.before ? findImportPathGroup(plugin, surrounding.before) : null
-      const afterGroup = surrounding.after ? findImportPathGroup(plugin, surrounding.after) : null
+      const beforeGroup = surrounding.before
+        ? findImportPathGroup(plugin, surrounding.before)
+        : null
+      const afterGroup = surrounding.after
+        ? findImportPathGroup(plugin, surrounding.after)
+        : null
       const newGroup = findImportPathGroup(plugin, importPath || exportName)
 
-      if (surrounding.before && newGroup != beforeGroup) newLine = '\n' + newLine
+      if (surrounding.before && newGroup != beforeGroup)
+        newLine = '\n' + newLine
       if (surrounding.after && newGroup != afterGroup) newLine += '\n'
     }
   }
-  
+
   plugin.utils.insertLine(newLine, linePosition, lines)
 }
 
 function findImportPathGroup(plugin, importPath) {
   const importPathPrefix = plugin.utils.strUntil(importPath, '.')
-  
+
   for (const group of plugin.importGroups) {
-    if (Array.isArray(group) ? group.includes(importPathPrefix) : group(importPathPrefix)) {
+    if (
+      Array.isArray(group)
+        ? group.includes(importPathPrefix)
+        : group(importPathPrefix)
+    ) {
       return group
     }
   }
 }
 
 function maybeParseLineImportPath(plugin, line) {
-  return line.startsWith('from') || line.startsWith('import') ? parseLineImportPath(plugin, line) : null
+  return line.startsWith('from') || line.startsWith('import')
+    ? parseLineImportPath(plugin, line)
+    : null
 }
 
 function getSurroundingImportPaths(plugin, lines, insertPosition) {
   let before
-  
+
   for (let i = insertPosition; i > -1; i--) {
     const line = lines[i].trim()
     if (!line || line.startsWith('#') || line.startsWith('"""')) continue // don't break in case comment is in the middle of a group
@@ -118,136 +148,73 @@ function getSurroundingImportPaths(plugin, lines, insertPosition) {
  * (resulting in lineIndexModifier = 0), or inserted as an entirely new import line before or after
  * (lineIndexModifier = -1 or 1)
  **/
-function getLinePosition(plugin, importPath, isExtraImport, lines) {
-  let start
-  let isMultiLine
-  let isInsideParens
+function getLinePosition(plugin, importPath, isExtraImport, imports, text) {
+  // If no imports, find first non-comment line
+  if (!imports.length) {
+    const lineIndex = getLastInitialCommentLine(text)
+    return {
+      start: lineIndex != null ? lineIndex : text.split('\n').length - 1,
+      lineIndexModifier: 1
+    }
+  }
+
+  // Imports exist, find correct sort order
+
   const importLineData = {}
-
-  for (let i = 0; i < lines.length; i++) {
-    const line = lines[i].trim()
-    if (!line) continue
-    
-    if (!isMultiLine) {
-      if (line.startsWith('import')) {
-        const linePath = parseLineImportPath(plugin, line)
-        if (linePath === importPath) return lineData // Return start/end data immediately if matching path found
-        importLineData[linePath] = {start: i}
-        continue
-      }
-
-      const isImportStart = line.startsWith('from')
-      if (!isImportStart && start == null) {
-        if (!line.startsWith('#')) break // no longer in import section
-        continue
-      }
-
-      if (isImportStart) start = i
-    }
-
-    if (isInsideParens) {
-      if (!line.endsWith(')')) continue
-    } else if (line.includes(' import (')) {
-      if (!line.endsWith(')')) {
-        isMultiLine = true
-        isInsideParens = true
-        continue
-      }
-    } else if (line.endsWith('\\')) {
-      isMultiLine = true
-      continue
-    }
-
-    isMultiLine = false
-    isInsideParens = false
-
-    const linePath = parseLineImportPath(plugin, lines[start])
-    const lineData = {start, end: i}
-    if (linePath === importPath) return lineData // Return start/end data immediately if matching path found
-    importLineData[linePath] = lineData
-    start = null
-  }
-
-  const paths = Reflect.ownKeys(importLineData)
-
-  // If this is the first import, find the first non-comment line.
-  if (!paths.length) {
-    // If there is no line that doesn't start with a comment, we need lineIndexModifier to be 1.
-    // It will get set to -1 if a line without a comment is encountered (see end of for-loop)
-    let lineIndexModifier = 1
-    let isMultilineComment
-    let lineIndex
-    
-    for (let i = 0; i < lines.length; i++) {
-      // Don't use lineIndex as incrementor in for-loop declaration because it will get incremented one time too many
-      lineIndex = i
-      const line = lines[i].trim()
-      if (isMultilineComment) {
-        if (line.endsWith('"""')) isMultilineComment = false
-        continue
-      }
-      if (line.startsWith('"""')) {
-        isMultilineComment = true
-        continue
-      }
-      if (line.startsWith('#')) continue
-      lineIndexModifier = -1
-      break
-    }
-
-    return { start: lineIndex, lineIndexModifier, isFirstImportLine: true }
-  }
-
   const importPos = plugin.importOrderMap[importPath]
   const importIsAbsolute = !importPath.startsWith('.')
-  
-  for (let i = 0; i < paths.length; i++) {
-    const linePath = paths[i]
 
-    // TODO: find import group for every one ????
-    
+  for (const importData of imports) {
     // plugin.importOrder check
-    const lineImportPos = plugin.importOrderMap[linePath]
-    if (importPos != null && (!lineImportPos || importPos < lineImportPos )) {
-      return { start: importLineData[linePath].start, lineIndexModifier: -1 }
+    const lineImportPos = plugin.importOrderMap[importData.path]
+    if (importPos != null && (!lineImportPos || importPos < lineImportPos)) {
+      return {
+        start: importLineData[importData.path].start,
+        lineIndexModifier: -1
+      }
     } else if (lineImportPos != null) {
       continue
     }
 
     // Package check
-    const lineIsPackage = isPathPackage(plugin, linePath)
+    const lineIsPackage = isPathPackage(plugin, importData.path)
 
-    if (isExtraImport && (!lineIsPackage || importPath < linePath)) {
-      return {start: importLineData[linePath].start, lineIndexModifier: -1}
+    if (isExtraImport && (!lineIsPackage || importPath < importData.path)) {
+      return {
+        start: importLineData[importData.path].start,
+        lineIndexModifier: -1
+      }
     } else if (lineIsPackage) {
       continue
     }
 
     // Absolute path check
-    const lineIsAbsolute = !linePath.startsWith('.')
-    if (importIsAbsolute && (!lineIsAbsolute || importPath < linePath)) {
-      return {start: importLineData[linePath].start, lineIndexModifier: -1}
+    const lineIsAbsolute = !importData.path.startsWith('.')
+    if (importIsAbsolute && (!lineIsAbsolute || importPath < importData.path)) {
+      return {
+        start: importLineData[importData.path].start,
+        lineIndexModifier: -1
+      }
     } else if (lineIsAbsolute) {
       continue
     }
   }
 
   // Since we didn't find a line to sort the new import before, it will go after the last import
-  const lastLineData = importLineData[_.last(paths)]
+  const lastLineData = importLineData[_.last(imports).path]
   return {
     start: lastLineData.end || lastLineData.start,
     lineIndexModifier: 1
   }
 }
 
-function getNewLineImports(lines, exportName, linePosition) {
-  const {start, end, lineIndexModifier, isFirstImportLine} = linePosition
+function getNewLineImports(imports, exportName, linePosition) {
+  const {start, end, lineIndexModifier} = linePosition
+  if (lineIndexModifier) return [exportName]
 
-  if (lineIndexModifier || isFirstImportLine) return [exportName]
-
-  const lineImports = getLineImports(lines, start, end)
+  const lineImports = imports(lines, start, end)
   if (lineImports.includes(exportName)) return
-  
+
   lineImports.push(exportName)
   return lineImports
 }
@@ -284,11 +251,12 @@ function getNewLine(plugin, importPath, imports) {
 
     if (newLength < maxImportLineLength) {
       line += newText
-      if (isLast) fullText += line
     } else {
       fullText += line + lineEndChar + '\n' + tabChar
       line = newText.trim()
     }
+
+    if (isLast) fullText += line
   })
 
   if (useParens) fullText += ')'
@@ -297,5 +265,5 @@ function getNewLine(plugin, importPath, imports) {
 
 module.exports = {
   buildImportItems,
-  insertImport,
+  insertImport
 }
