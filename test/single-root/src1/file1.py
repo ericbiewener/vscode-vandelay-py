4a54b319a2edab719cf4d13f9c321947bb4564bc
Eric Biewener <eric@talentpair.com>
2018-07-24 22:15:56 -0700

    Starting tests

---
+++ test/single-root/src1/file1.py
@@ -0,0 +1,15 @@
+import full_package1
+from package3 import package3_1, package3_2, package3_3
+from package4 import (package4_1, package4_2,
+package4_3)
+from package5 import (
+  package5_1,
+  package5_2,
+  package5_3,
+)
+import full_package2
+ 
+from group1.subdir_1 import group1_1
+from group2.subdir_1 import group2_1
+from group1.subdir_2 import group1_2
+
